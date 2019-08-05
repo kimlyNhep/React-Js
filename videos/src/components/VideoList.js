76ea376a40cef9969ce1a477282a37ca4616f783
Kimly Nhep <kimly.nhep0168@gmail.com>
2019-08-05 13:24:27 +0700

    video

--- videos/src/components/VideoList.js
+++ videos/src/components/VideoList.js
@@ -5,7 +5,7 @@ const VideoList = ({ videos }) => {
   const renderVideo = videos.map(video => {
     return <VideoItem video={video} />;
   });
-  return <div>{renderVideo}</div>;
+  return <div className="ui relaxed divided list">{renderVideo}</div>;
 };
 
 export default VideoList;
