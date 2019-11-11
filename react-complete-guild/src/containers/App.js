479aacd0e8fc9958ba977453fb4ff8f2ccea6206
Your Name <kimly.nhep0168@gmail.com>
2019-11-11 18:19:48 +0700

    done

--- react-complete-guild/src/containers/App.js
+++ react-complete-guild/src/containers/App.js
@@ -1,22 +1,22 @@
-import React, { Component } from "react";
-import "./App.css";
-import Persons from "../components/Persons/Persons";
+import React, { Component } from 'react';
+import './App.css';
+import Persons from '../components/Persons/Persons';
 
 class App extends Component {
   state = {
     persons: [
-      { id: "001", name: "Max", age: 21 },
-      { id: "002", name: "Mary", age: 34 },
-      { id: "003", name: "Stev", age: 25 }
+      { id: '001', name: 'Max', age: 21 },
+      { id: '002', name: 'Mary', age: 34 },
+      { id: '003', name: 'Stev', age: 25 }
     ]
   };
 
   switchNameHandler = newName => {
     this.setState({
       persons: [
-        { id: "001", name: newName, age: 21 },
-        { id: "001", name: "Mary", age: 24 },
-        { id: "001", name: "Mark", age: 29 }
+        { id: '001', name: newName, age: 21 },
+        { id: '001', name: 'Mary', age: 24 },
+        { id: '001', name: 'Mark', age: 29 }
       ],
       showPerson: false
     });
@@ -53,12 +53,12 @@ class App extends Component {
 
   render() {
     const style = {
-      backgroundColor: "green",
-      color: "white",
-      font: "inherite",
-      border: "1px solid blue",
-      padding: "8px",
-      cursor: "pointer"
+      backgroundColor: 'green',
+      color: 'white',
+      font: 'inherite',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
     };
 
     var persons = null;
@@ -73,14 +73,31 @@ class App extends Component {
           />
         </div>
       );
-      style.backgroundColor = "red";
+      style.backgroundColor = 'red';
     }
-    return <div className="App">{persons}</div>;
-    // return React.createElement(
-    //   "div",
-    //   { className: "App" },
-    //   React.createElement("h1", null, "Hi There!")
-    // );
+
+    const classes = [];
+    if (this.state.persons.length <= 2) {
+      classes.push('red'); // classes = ['red']
+    }
+    if (this.state.persons.length <= 1) {
+      classes.push('bold');
+    }
+
+    return (
+      <div className='App'>
+        <h1>Hi There!</h1>
+        <p className={classes.join(' ')}>This is really working!</p>
+        <button
+          className='button'
+          style={style}
+          onClick={this.togglePersonHandler}
+        >
+          Toggle Persons
+        </button>
+        {persons}
+      </div>
+    );
   }
 }
 
