502e1ed7729c044163e9966df775ffacf619b224
kimly Nhep <kimly.nhep0168@gmail.com>
2019-11-25 14:39:50 +0700

    done

--- guild/src/index.js
+++ guild/src/index.js
@@ -4,14 +4,10 @@ import "./index.css";
 import App from "./containers/App";
 import * as serviceWorker from "./serviceWorker";
 
-<<<<<<< HEAD
-ReactDOM.render(<App />, document.getElementById("root"));
-=======
 ReactDOM.render(
-  <App appTitle='Person Mananger' />,
-  document.getElementById('root')
+  <App appTitle="Person Mananger" />,
+  document.getElementById("root")
 );
->>>>>>> 3c47a62bf22f9089fd2564f33389ff861968c8b6
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
